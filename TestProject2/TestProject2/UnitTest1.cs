a137e1c333c00d7089859e9276bd4fee7d2299d0
Mark Joseph Caparas <markjoseph.caparas@ph.softvision.global>
2022-10-11 00:50:50 +0800

    Completed

--- TestProject2/TestProject2/UnitTest1.cs
+++ TestProject2/TestProject2/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using System.Text;
 using System.Net.Http;
+using System.Net;
 
 
 namespace TestProject2
@@ -34,14 +35,32 @@ namespace TestProject2
             ModelForPet petData = new ModelForPet()
             {
                 id = 1234,
+                category =  new Category() {
+                    id = 5678,
+                    name = "PetCategory" },
+                
+               
                 name = "Blacky",
                 photoUrls = new string[]
                 {
                     "Running", "Sleeping"
-                }
+                },
                 
+                tags = new Tag[]
+                {
+                    new Tag() 
+                    {
+                     id = 0,
+                     name = "PetTag" 
 
-                
+                    }
+                    
+
+                },
+                status = "available"
+
+
+
             };
 
             // Serialize Content
@@ -50,11 +69,115 @@ namespace TestProject2
 
             // Send Post Request
             await httpClient.PostAsync(GetURL(PetEndpoint), postRequest);
+
+            #region get pet of the created data
+
+            // Get Request
+            var getResponse = await httpClient.GetAsync(GetURI($"{PetEndpoint}/{petData.id}"));
+
+            // Deserialize Content
+            var listPetData = JsonConvert.DeserializeObject<ModelForPet>(getResponse.Content.ReadAsStringAsync().Result);
+
+            // filter created data
+            var createdPetData = listPetData.name;
+
+            #endregion
+
+            #region send put request to update data
+
+            // Update Json Object
+             petData = new ModelForPet()
+            {
+                id = 1234,
+                category = new Category()
+                {
+                    id = 5678,
+                    name = "PetCategory"
+                },
+
+
+                name = "Whity",
+                photoUrls = new string[]
+                {
+                    "Running", "Sleeping"
+                },
+
+                tags = new Tag[]
+                {
+                    new Tag()
+                    {
+                     id = 0,
+                     name = "PetTag"
+
+                    }
+
+
+                },
+                status = "available"
+
+
+
+            };
+            
+
+            // Serialize Content
+            request = JsonConvert.SerializeObject(petData);
+            postRequest = new StringContent(request, Encoding.UTF8, "application/json");
+
+            // Send Put Request
+            var httpResponse = await httpClient.PutAsync(GetURL($"{PetEndpoint}"), postRequest);
+
+            // Get Status Code
+            var statusCode = httpResponse.StatusCode;
+
+            #endregion
+
+            #region get updated data
+
+            // Get Request
+            getResponse = await httpClient.GetAsync(GetURI($"{PetEndpoint}/{petData.id}"));
+
+            // Deserialize Content
+            listPetData = JsonConvert.DeserializeObject<ModelForPet>(getResponse.Content.ReadAsStringAsync().Result);
+
+            // filter created data
+            createdPetData = listPetData.name;
+
+            #endregion
+
+            #region cleanup data
+
+            // Add data to cleanup list
+            cleanUpList.Add(listPetData);
+
+            #endregion
+
+            #region assertion
+
+            // Assertion
+            Assert.AreEqual(HttpStatusCode.OK, statusCode, "Status code is not equal to 201");
+            Assert.AreEqual(petData.name, createdPetData, "Petdata is not matching");
+
+            #endregion
         }
+
+
+
 
         [TestMethod]
         public void TestMethod2()
         {
         }
+
+
+        [TestCleanup]
+        public async Task TestCleanUp()
+        {
+            foreach (var data in cleanUpList)
+            {
+                var httpResponse = await httpClient.DeleteAsync(GetURL($"{PetEndpoint}/{data.id}"));
+            }
+        }
+
     }
 }
